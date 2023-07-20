6148afe374cb9d3992656f9c5402a59ed8c71779
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-07-20 00:38:01 -0700

    header component

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import "assets/header.scss";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+    return (
+        <header>
+            <Link to={`/`}>Home</Link>
+            <Link to={`popular`}>Popular</Link>
+            <Link to={`random`}>Random</Link>
+        </header>
+    );
+}
