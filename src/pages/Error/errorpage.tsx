3a60cf7c1af59a1ac638c159f5d9344916472a27
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-07-24 04:01:20 -0700

    fixing the errorpage with typescript

--- src/pages/Error/errorpage.tsx
+++ src/pages/Error/errorpage.tsx
@@ -1,10 +1,23 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, isRouteErrorResponse } from "react-router-dom";
 import "../../assets/error.scss";
 import Header from "../../components/header";
 import { Link } from "react-router-dom";
 
+function errorMessage(error: unknown): string {
+    if (isRouteErrorResponse(error)) {
+        return `${error.status} ${error.statusText}`;
+    } else if (error instanceof Error) {
+        return error.message;
+    } else if (typeof error === "string") {
+        return error;
+    } else {
+        console.error(error);
+        return "Unknown error";
+    }
+}
+
 export default function ErrorPage() {
-    // const error = useRouteError();
+    const message = errorMessage(useRouteError());
 
     return (
         <div className="errorpage">
@@ -12,7 +25,9 @@ export default function ErrorPage() {
             <section>
                 <h1>Error!</h1>
                 <h2>Sorry, an unexpected error has occurred.</h2>
-                <h3>{/* <i>{error.statusText || error.message}</i> */}</h3>
+                <h3>
+                    <i>{message}</i>
+                </h3>
                 <Link to={`/`}>
                     <button>Back to home</button>
                 </Link>
