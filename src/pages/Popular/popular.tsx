5b43b6c4e04f5bfe9f6b764ef59cecf29ca31d0a
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-07-25 18:40:16 -0700

    basic popular page form handling

--- src/pages/Popular/popular.tsx
+++ src/pages/Popular/popular.tsx
@@ -1,10 +1,110 @@
 import Header from "components/header";
+import { Fragment, useState, Dispatch } from "react";
+import "assets/popular.scss";
+
+const query_options = [
+    { type: "type", options: ["tv", "movie", "ova", "special", "ona", "music"] },
+    { type: "filter", options: ["airing", "upcoming", "bypopularity", "favorite"] }
+];
+
+type OptionState = {
+    type: string;
+    option: string;
+};
+
+function createOptionState(): OptionState[] {
+    return query_options.map((item) => {
+        return { type: item.type, option: item.options[0] }; // use first option
+    });
+}
+
+export function Option({
+    type,
+    option_index,
+    options,
+    data,
+    fullData,
+    setDataCallback
+}: {
+    type: string;
+    option_index: number;
+    options: string[];
+    data: OptionState;
+    fullData: OptionState[];
+    setDataCallback: Dispatch<OptionState[]>;
+}) {
+    let newfullData = fullData.map((item) => {
+        return {
+            ...item
+        };
+    }); //clone the full data
+
+    return (
+        <fieldset className="option">
+            <p>{type}</p>
+            {options.map((item) => {
+                return (
+                    //each input in action
+                    <Fragment key={item}>
+                        <input
+                            type="radio"
+                            id={item}
+                            value={item}
+                            name={type}
+                            defaultChecked={data.option === item}
+                            onChange={() => {
+                                newfullData[option_index].option = item;
+                                setDataCallback(newfullData);
+                            }}
+                        />
+                        <label htmlFor={item}>{item}</label>
+                    </Fragment>
+                );
+            })}
+        </fieldset>
+    );
+}
+
+export function OptionsForm({
+    data,
+    setDataCallback
+}: {
+    data: OptionState[];
+    setDataCallback: Dispatch<OptionState[]>;
+}) {
+    return (
+        <form
+            className="popular_options_form"
+            onSubmit={(e) => {
+                e.preventDefault();
+            }}
+        >
+            {query_options.map((item, index) => {
+                // for each option
+                return (
+                    <Option
+                        key={item.type}
+                        {...item}
+                        fullData={data}
+                        data={data[index]}
+                        setDataCallback={setDataCallback}
+                        option_index={index}
+                    />
+                );
+            })}
+        </form>
+    );
+}
+
+// TODO - improve state logic
 
 export default function Popular() {
+    let [data, setData] = useState<OptionState[]>(createOptionState());
+
     return (
-        <div>
+        <div className="popular">
             <Header></Header>
-            Popular page
+            <OptionsForm data={data} setDataCallback={setData} />
         </div>
     );
 }
