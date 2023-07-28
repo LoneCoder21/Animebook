02b93f84ed6c6d883571ca25df878ab179036d44
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-07-28 00:06:35 -0700

    some basic text input field styling

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,18 @@
 import "assets/pages/Home/home.scss";
+import "assets/input/searchfield.scss";
 import Recommendations from "./Recommendations";
+import SearchForm from "./SearchForm";
 import Header from "components/header";
 
 export default function Home() {
     return (
         <div>
             <Header />
-            <Recommendations></Recommendations>
+
+            <div className="home">
+                <SearchForm />
+                <Recommendations />
+            </div>
         </div>
     );
 }
