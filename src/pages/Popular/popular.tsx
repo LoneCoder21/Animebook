ae3493eafa7695431fb6e275dc580e797dae1074
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-08-07 15:35:09 -0700

    grid to flex for popular page

--- src/pages/Popular/popular.tsx
+++ src/pages/Popular/popular.tsx
@@ -107,7 +107,7 @@ export default function Popular() {
             <OptionsForm data={data} setDataCallback={setData} />
             <div className="grid_container">
                 {animes.map((item) => {
-                    return <Recommendation key={item.title} {...item} width="83%" />;
+                    return <Recommendation key={item.title} {...item} width="100%" />;
                 })}
             </div>
         </div>
