43933c6a373b67dd3fde1b65db6caf11687239bc
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-08-07 19:03:19 -0700

    improving header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,49 @@
 import "assets/header.scss";
 import { Link } from "react-router-dom";
 import { BsGithub } from "react-icons/bs";
+import { FaHome, FaRandom } from "react-icons/fa";
+import { AiFillHeart } from "react-icons/ai";
+
+export function HeaderItem({
+    to,
+    name,
+    children,
+    newtab = false
+}: {
+    to: string;
+    name: string;
+    newtab?: boolean;
+    children: JSX.Element;
+}) {
+    return (
+        <Link className="link" to={to} target={newtab ? "_blank" : "_self"} rel="noopener noreferrer">
+            {children}
+            <span className={name}>{name}</span>
+        </Link>
+    );
+}
 
 export default function Header() {
     return (
         <header>
             <div className="left">
-                <Link to={`/`}>Home</Link>
-                <Link to={`/popular`}>Popular</Link>
-                <Link to={`/random`}>Random</Link>
+                <HeaderItem to="/" name="Home">
+                    <FaHome className="icon" size={25} />
+                </HeaderItem>
+                <HeaderItem to="/popular" name="Popular">
+                    <AiFillHeart className="icon" size={25} />
+                </HeaderItem>
+                <HeaderItem to="/random" name="Random">
+                    <FaRandom className="icon" size={25} />
+                </HeaderItem>
             </div>
             <div className="right">
-                <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+                <HeaderItem to="https://github.com/LoneCoder21/Animebook" name="Github" newtab={true}>
                     <BsGithub className="icon" size={25} />
-                </a>
+                </HeaderItem>
+                <HeaderItem to="https://jikan.moe/" name="Jikan" newtab={true}>
+                    <></>
+                </HeaderItem>
             </div>
         </header>
     );
