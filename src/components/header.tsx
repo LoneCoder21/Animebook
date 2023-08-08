b216c97639aa1762c25e2fe53e8400823c335897
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-08-07 17:29:07 -0700

    header modifications

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,20 @@
 import "assets/header.scss";
 import { Link } from "react-router-dom";
+import { BsGithub } from "react-icons/bs";
 
 export default function Header() {
     return (
         <header>
-            <Link to={`/`}>Home</Link>
-            <Link to={`/popular`}>Popular</Link>
-            <Link to={`/random`}>Random</Link>
+            <div className="left">
+                <Link to={`/`}>Home</Link>
+                <Link to={`/popular`}>Popular</Link>
+                <Link to={`/random`}>Random</Link>
+            </div>
+            <div className="right">
+                <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+                    <BsGithub className="icon" size={25} />
+                </a>
+            </div>
         </header>
     );
 }
