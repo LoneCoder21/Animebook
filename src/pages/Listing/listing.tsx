e9c57b10ad802b1a8e913edb0bcd534bb80c6c2f
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-08-18 01:06:16 -0700

    basic listing functionality with no css

--- src/pages/Listing/listing.tsx
+++ src/pages/Listing/listing.tsx
@@ -11,7 +11,7 @@ export class ListingData {
     episodes?: number;
     duration: string;
     rating: string;
-    synopsis: string;
+    synopsis?: string;
     genres: string[];
     themes: string[];
 
@@ -79,11 +79,106 @@ export class ListingData {
     }
 }
 
-export function Listing({ data }: { data: ListingData }) {
+function ListingCard({ data }: { data: ListingData }) {
+    return (
+        <div>
+            <h3>{data.title}</h3>
+            <img src={data.image} alt={data.title} />
+        </div>
+    );
+}
+
+function Synopsis({ data }: { data: ListingData }) {
+    return (
+        <div>
+            <h4>Synopsis</h4>
+            {data.synopsis ? data.synopsis : "No synopsis found"}
+        </div>
+    );
+}
+
+function Premiere({ data }: { data: ListingData }) {
+    return (
+        <div>
+            <h3>{data.premiere && data.premiere?.season + " " + data.premiere?.year}</h3>
+        </div>
+    );
+}
+
+function Type({ data }: { data: ListingData }) {
+    return (
+        <div>
+            <h3>{data.type}</h3>
+        </div>
+    );
+}
+
+function Rating({ data }: { data: ListingData }) {
+    return (
+        <div>
+            <p>{data.rating}</p>
+        </div>
+    );
+}
+
+function Tags({ data }: { data: ListingData }) {
+    return (
+        <div>
+            {data.genres.map((genre) => {
+                return <p key={genre}>{genre}</p>;
+            })}
+        </div>
+    );
+}
+
+function Score({ data }: { data: ListingData }) {
+    return <div>{data.stats.score && "Score - " + data.stats.score}</div>;
+}
+
+function Rank({ data }: { data: ListingData }) {
+    return <div>{data.stats.rank && "Rank - " + data.stats.rank}</div>;
+}
+
+function Popularity({ data }: { data: ListingData }) {
+    return <div>{data.stats.popularity && "Popularity - " + data.stats.popularity}</div>;
+}
+
+function Info({ data }: { data: ListingData }) {
+    return (
+        <div>
+            <p>{"Episodes - " + (data.episodes ? "Unknown" : data.episodes)}</p>
+            <p>{"Duration - " + data.duration}</p>
+            <p>{"Rating - " + data.rating}</p>
+
+            <p>{"Status - " + data.airing.status}</p>
+            <p>{"Airing - " + data.airing.airing}</p>
+            <p>{"Air Date - " + data.airing.description}</p>
+        </div>
+    );
+}
+
+function Trailer({ data }: { data: ListingData }) {
+    if (data.trailer == null) {
+        return <></>;
+    }
+    return <iframe src={data.trailer?.embed} />;
+}
+
+function Listing({ data }: { data: ListingData }) {
     return (
         <div>
             <Header />
-            {data.id}
+            <ListingCard data={data} />
+            <Synopsis data={data} />
+            <Premiere data={data} />
+            <Type data={data} />
+            <Rating data={data} />
+            <Tags data={data} />
+            <Score data={data} />
+            <Rank data={data} />
+            <Popularity data={data} />
+            <Info data={data} />
+            <Trailer data={data} />
         </div>
     );
 }
@@ -106,7 +201,6 @@ export default function ListingEntry() {
             })
             .then((data) => {
                 setLoading(false);
-                const j = ListingData.fromJson(data["data"]);
                 setListingData(ListingData.fromJson(data["data"]));
             })
             .catch((error) => {
