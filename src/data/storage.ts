31f8dc5a3369dc299930588e27812bc7866f9661
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-08-25 22:18:55 -0700

    fix local storage availability bug

--- src/data/storage.ts
+++ src/data/storage.ts
@@ -1,12 +1,18 @@
 export function getLocalStorage<T>(key: string, normal: T): T {
-    let storage = localStorage.getItem(key);
-    if (storage == null) return normal;
-    const json = JSON.parse(storage) as T;
-    if (!json) return normal;
-    return json;
+    try {
+        let storage = localStorage.getItem(key);
+        if (storage == null) return normal;
+        const json = JSON.parse(storage) as T;
+        if (!json) return normal;
+        return json;
+    } catch (e) {
+        return normal;
+    }
 }
 
 export function setLocalStorage<T>(key: string, value: T): void {
-    const json_str = JSON.stringify(value);
-    localStorage.setItem(key, json_str);
+    try {
+        const json_str = JSON.stringify(value);
+        localStorage.setItem(key, json_str);
+    } catch (e) {}
 }
